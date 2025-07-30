6a8be78ea306f2944c39b6a538951ab4f260275a
Cast3c <ricardo.castellarp@gmail.com>
2025-07-30 17:10:15 -0500

    fix deployment II

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Cast3c/portfolio_react',
+  base: '/portfolio_react',
   plugins: [react()]
 })
